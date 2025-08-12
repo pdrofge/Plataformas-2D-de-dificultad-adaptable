80d1f2819f7540dcadb7d62aa34bf7b125c2e106
pdrofge <pdrofge@gmail.com>
2025-08-13 00:30:59 +0200

    nivel 1 implementado a falta del fondo y el finish de cada camino
    nivel 1 implementado a falta del fondo y el finish de cada camino. Funciona a medias el finish del primer camino ( el difícil)

--- Scenes/levels/levels/end_of_level.gd
+++ Scenes/levels/levels/end_of_level.gd
@@ -11,7 +11,7 @@ func _ready() -> void:
 func _physics_process(delta) -> void:
 	if floor_ray and floor_ray.is_colliding():
 		var collider: Node = floor_ray.get_collider()
-		if collider.name == "end_of_level":
+		if collider.name == "end_of_level_1":
 			GameManager.set_lifes(player.lifes, player.max_lifes)
 			GameManager.set_last_scene(get_tree().current_scene.scene_file_path)
 			var tree = get_tree()
